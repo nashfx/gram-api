f3d32ee8fe0c31eb46dd52659ae80b6e576afc7d
Daniel Duque <danielvduque@gmail.com>
2019-02-15 13:17:41 -0300

    add token. get self user last media

--- routes/index.js
+++ routes/index.js
@@ -2,29 +2,19 @@ var express = require('express');
 var router = express.Router();
 const https = require("https");
 const _ = require('lodash');
+const request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	//https://www.instagram.com/oauth/authorize/?client_id=fc0bee7330524cb9ba1f5b2bbf2d0a7c&redirect_uri=http://localhost:3300&response_type=token&scope=public_content
-	const url = `https://www.instagram.com/oauth/authorize/?client_id=${process.env.CLIENTID}&redirect_uri=${process.env.REDIRECT}&response_type=token&scope=public_content`;
-	console.log(url);
+	request.get(`https://api.instagram.com/v1/users/self/media/recent?access_token=${process.env.TOKEN}`, (err, response, body) => {
+        if (err) {
+            return next(err);
+        }
 
-	let body = "";
-
-	https.get(url, response => {
-  		response.setEncoding("utf8");
-
-  		response.on("data", data => {
-    		body += data;
-  		});
-
-  		response.on("end", () => {
-    		body = JSON.parse(body);
-    		console.log(body);
-  		});
-	});
-
-  	res.render('index', { title: 'Express' });
-});
+        res.json(JSON.parse(body));
+    });
+	// get popular photos
+	// api.instagram.com/v1/media/popular?access_token=youraccesstoken
+}); 
 
 module.exports = router;
